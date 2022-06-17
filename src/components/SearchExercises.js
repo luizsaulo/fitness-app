1e729dce108891e03991c14ef4f6c9b3afb52b34
Luiz <luizsaulo@yahoo.com.br>
2022-06-17 11:19:57 -0300

    building the horizontal scroolbar that will display wht exercise groups

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -4,12 +4,11 @@ import { exerciseOptions, fetchData } from '../utils/fetchData';
 import HorizontalScrollbar from './HorizontalScrollbar';
 
 const SearchExercises = () => {
-  const [search, setSearch] = useState('')
-  const [exercises, setExercises] = useState([]);
+  const [search, setSearch] = useState('')  
   const [bodyParts, setBodyParts] = useState([]);
 
   useEffect(() => {
-    const fetchEcercisesData = async () => {
+    const fetchEcercisesData = async ({ setExercises, bodyPart, setBodyPart }) => {
       const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
 
       setBodyParts(['all', ...bodyPartsData]);
@@ -71,7 +70,11 @@ const SearchExercises = () => {
         </Button>
       </Box>
       <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
-          <HorizontalScrollbar data={bodyParts} />
+          <HorizontalScrollbar 
+            data={bodyParts}
+            bodyPart={bodyPart} 
+            setBodyPart={setBodyPart}
+          />
       </Box>
     </Stack>
   )
