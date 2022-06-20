bfb8053286437d1f4b9229aa4eb41a634d0d25b0
Luiz <luizsaulo@yahoo.com.br>
2022-06-20 11:45:59 -0300

    finished the useeffect functionality - now it loads the exercises according to the group you click. It displays the exercises rendered below the muscle group card

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -9,10 +9,28 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const exercisesPerPage = 9;
 
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
+
   const paginate =(e, value) => {
     setCurrentPage(value);
     window.scrollTo({ top: 1800, behavior: 'smooth' })
   }
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if(bodyPart === 'all') {
+        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      } else {
+        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodypart/${bodyPart}`, exerciseOptions);
+      }
+      setExercises(exercisesData);
+    }
+    fetchExercisesData();
+  }, [bodyPart]);
 
   return (
     <Box id='exercises'
@@ -24,7 +42,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
         Showing Results
       </Typography>
       <Stack direction='row' sx={{ gap: { lg: '110px', xs: '50px' }}} flexWrap='wrap' justifyContent='center'>
-        {exercises.map((exercise, index) => (
+        {currentExercises.map((exercise, index) => (
           <ExerciseCard key={index} exercise={exercise} />
         ))}
       </Stack>
